67bd85087983e90b32de914862f8c86941ed0160
Yaghuob Shahmari <shahmari.acer@outlook.com>
2025-08-04 23:50:18 +0900

    Add latency multiplier L to model equations
    Introduces a new parameter L to the Parameters struct and updates all model equations to multiply latency rates by L. This change allows for flexible adjustment of latency stage durations in the malaria transmission model.

--- src/basic_reproduction_number_new_model.jl
+++ src/basic_reproduction_number_new_model.jl
@@ -45,6 +45,7 @@ mutable struct Parameters
     s2M::Float64
     s1T::Float64
     s2T::Float64
+    L::Int64
 end
 
 """
@@ -61,8 +62,9 @@ function compute_dfe(par::Parameters)
         SM, S2T = vars
         a = par.a; b = par.b; c = par.c; m = par.m;
         r = par.r; g = par.g; h1 = par.h1; h2 = par.h2;
-        ε = par.ε; p = par.p; s1M = par.s1M;
-        s2M = par.s2M; s1T = par.s1T; s2T = par.s2T
+        ε = par.ε; p = par.p; s1M = par.s1M; L = par.L;
+        s2M = par.s2M; s1T = par.s1T; s2T = par.s2T;
+
         S1T = ε * p * a * (SM + S2T) / (h1 + g)
         F[1] = g + h2 * S2T - ((ε * p * a + g) * SM)
         F[2] = h1 * S1T - ((ε * p * a * S2T + h2 + g) * S2T)
@@ -88,8 +90,9 @@ function compute_dfe_closed_form(par::Parameters)
     IH = 0.0
     a = par.a; b = par.b; c = par.c; m = par.m;
     r = par.r; g = par.g; h1 = par.h1; h2 = par.h2;
-    ε = par.ε; p = par.p; s1M = par.s1M;
-    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T
+    ε = par.ε; p = par.p; s1M = par.s1M; L = par.L;
+    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T;
+
     # Closed-form treated compartments
     S1T = ε * p * a / (ε * p * a + h1 + g)
     S2T = (h1 / (ε * p * a + h2 + g)) * S1T
@@ -108,25 +111,27 @@ function compute_R0(par::Parameters)
     SH, SM = dfe.SH, dfe.SM
     a = par.a; b = par.b; c = par.c; m = par.m;
     r = par.r; g = par.g; h1 = par.h1; h2 = par.h2;
-    ε = par.ε; p = par.p; s1M = par.s1M;
-    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T
+    ε = par.ε; p = par.p; s1M = par.s1M; L = par.L;
+    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T;
+    
+    # Construct the next-generation matrix K
     F = zeros(7, 7)
     V = zeros(7, 7)
     F[1, 4] = m * (1 - p) * a * b * SH
     F[1, 7] = m * (1 - p) * a * b * SH
     F[2, 1] = (1 - p) * a * c * SM
     V[1, 1] = r
-    V[2, 2] = ε * p * a + s1M + g
-    V[3, 3] = s2M + g
-    V[3, 2] = -s1M
+    V[2, 2] = ε * p * a + L * s1M + g
+    V[3, 3] = L * s2M + g
+    V[3, 2] = -L * s1M
     V[4, 4] = g
-    V[4, 3] = -s2M
-    V[5, 5] = s1T + g
+    V[4, 3] = -L * s2M
+    V[5, 5] = L * s1T + g
     V[5, 2] = -ε * p * a
-    V[6, 6] = s2T + g
-    V[6, 5] = -s1T
+    V[6, 6] = L * s2T + g
+    V[6, 5] = -L * s1T
     V[7, 7] = g
-    V[7, 6] = -s2T
+    V[7, 6] = -L * s2T
     K = F * (V \ I)
     return maximum(real(eigvals(K)))
 end
@@ -135,22 +140,24 @@ function compute_R0_closed_form(par::Parameters)
     # Closed-form R0 from the report
     a = par.a; b = par.b; c = par.c; m = par.m;
     r = par.r; g = par.g; h1 = par.h1; h2 = par.h2;
-    ε = par.ε; p = par.p; s1M = par.s1M;
-    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T
+    ε = par.ε; p = par.p; s1M = par.s1M; L = par.L;
+    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T;
+
     # Survival probability through 2-stage untreated latency
-    P = (s1M / (s1M + g)) * (s2M / (s2M + g))
+    P = (L * s1M / (L * s1M + g)) * (L * s2M / (L * s2M + g))
     # Baseline R0 without treatment
     R0_bar = P * m * a^2 * b * c / (g * r)
     # Probability of acquiring treatment during latency (two stages): rate εpa over total exit rate
-    pi_T = ε * p * a / (ε * p * a + s1M + g)
+    pi_T = ε * p * a / (ε * p * a + L * s1M + g)
     # Survival probability through 2-stage treated latency
-    P_T = (s1T / (s1T + g)) * (s2T / (s2T + g))
+    P_T = (L * s1T / (L * s1T + g)) * (L * s2T / (L * s2T + g))
     # Factor phi_T = relative survival under treatment vs untreated
     phi_T = P_T / P
     # DFE susceptible untreated mosquito fraction
     SM = compute_dfe_closed_form(par).SM
     # Combine according to Eq.(treated_R0)
-    return (((1 - pi_T) + phi_T * pi_T) * (1 - p)^2 * SM * R0_bar)
+    R0 = (((1 - pi_T) + phi_T * pi_T) * (1 - p)^2 * SM * R0_bar)
+    return (R0=R0, R0_bar=R0_bar, pi_T=pi_T, phi_T=phi_T)
 end
 
 """
@@ -162,20 +169,12 @@ function compute_endemic_equilibrium(par::Parameters)
     # if R0 ≤ 1
     #     return nothing
     # end
-    a = par.a
-    b = par.b
-    c = par.c
-    m = par.m
-    r = par.r
-    g = par.g
-    h1 = par.h1
-    h2 = par.h2
-    ε = par.ε
-    p = par.p
-    s1M = par.s1M
-    s2M = par.s2M
-    s1T = par.s1T
-    s2T = par.s2T
+    a = par.a; b = par.b; c = par.c; m = par.m;
+    r = par.r; g = par.g; h1 = par.h1; h2 = par.h2;
+    ε = par.ε; p = par.p; s1M = par.s1M; L = par.L;
+    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T;
+    
+    # Compute the endemic equilibrium
     function g!(G, vars)
         SM, S2T = vars
         S1T = ε * p * a * (SM + S2T) / (h1 + g)
@@ -191,12 +190,12 @@ function compute_endemic_equilibrium(par::Parameters)
         end
         sol = nlsolve(g!, [par.g, 0.0])
         SM, S2T = sol.zero
-        E1M = (1 - p) * a * c * IH * SM / (ε * p * a + s1M + g)
-        E2M = s1M * E1M / (s2M + g)
-        IM = s2M * E2M / g
-        E1T = ε * p * a * E1M / (s1T + g)
-        E2T = s1T * E1T / (s2T + g)
-        IT = s2T * E2T / g
+        E1M = (1 - p) * a * c * IH * SM / (ε * p * a + L * s1M + g)
+        E2M = L * s1M * E1M / (L * s2M + g)
+        IM = L * s2M * E2M / g
+        E1T = ε * p * a * E1M / (L * s1T + g)
+        E2T = L * s1T * E1T / (L * s2T + g)
+        IT = L * s2T * E2T / g
         return m * (1 - p) * a * b * (IM + IT) * (1 - IH) - r * IH
     end
     lo, hi = 1e-6, 1 - 1e-6
@@ -213,12 +212,12 @@ function compute_endemic_equilibrium(par::Parameters)
     sol = nlsolve((G, v) -> g!(G, v), [par.g, 0.0])
     SM, S2T = sol.zero
     S1T = ε * p * a * (SM + S2T) / (h1 + g)
-    E1M = (1 - p) * a * c * IH * SM / (ε * p * a + s1M + g)
-    E2M = s1M * E1M / (s2M + g)
-    IM = s2M * E2M / g
-    E1T = ε * p * a * E1M / (s1T + g)
-    E2T = s1T * E1T / (s2T + g)
-    IT = s2T * E2T / g
+    E1M = (1 - p) * a * c * IH * SM / (ε * p * a + L * s1M + g)
+    E2M = L * s1M * E1M / (L * s2M + g)
+    IM = L * s2M * E2M / g
+    E1T = ε * p * a * E1M / (L * s1T + g)
+    E2T = L * s1T * E1T / (L * s2T + g)
+    IT = L * s2T * E2T / g
     return (SH=SH, IH=IH, SM=SM, E1M=E1M, E2M=E2M, IM=IM,
         S1T=S1T, S2T=S2T, E1T=E1T, E2T=E2T, IT=IT)
 end
@@ -229,31 +228,23 @@ State vector: [SH,...,IT]
 """
 function malaria_ode!(du, u, par::Parameters, t)
     SH, IH, SM, E1M, E2M, IM, S1T, S2T, E1T, E2T, IT = u
-    a = par.a
-    b = par.b
-    c = par.c
-    m = par.m
-    r = par.r
-    g = par.g
-    h1 = par.h1
-    h2 = par.h2
-    ε = par.ε
-    p = par.p
-    s1M = par.s1M
-    s2M = par.s2M
-    s1T = par.s1T
-    s2T = par.s2T
+    a = par.a; b = par.b; c = par.c; m = par.m;
+    r = par.r; g = par.g; h1 = par.h1; h2 = par.h2;
+    ε = par.ε; p = par.p; s1M = par.s1M; L = par.L;
+    s2M = par.s2M; s1T = par.s1T; s2T = par.s2T;
+    
+    # Compute the derivatives
     du[1] = -m * (1 - p) * a * b * (IM + IT) * SH + r * IH
     du[2] = m * (1 - p) * a * b * (IM + IT) * SH - r * IH
     du[3] = g + h2 * S2T - (((1 - p) * a * c * IH) + ε * p * a + g) * SM
-    du[4] = (1 - p) * a * c * IH * SM - (ε * p * a + s1M + g) * E1M
-    du[5] = s1M * E1M - (s2M + g) * E2M
-    du[6] = s2M * E2M - g * IM
+    du[4] = (1 - p) * a * c * IH * SM - (ε * p * a + L * s1M + g) * E1M
+    du[5] = L * s1M * E1M - (L * s2M + g) * E2M
+    du[6] = L * s2M * E2M - g * IM
     du[7] = ε * p * a * (SM + S2T) - (h1 + g) * S1T
     du[8] = h1 * S1T - ((ε * p * a) * S2T + h2 + g) * S2T
-    du[9] = ε * p * a * E1M - (s1T + g) * E1T
-    du[10] = s1T * E1T - (s2T + g) * E2T
-    du[11] = s2T * E2T - g * IT
+    du[9] = ε * p * a * E1M - (L * s1T + g) * E1T
+    du[10] = L * s1T * E1T - (L * s2T + g) * E2T
+    du[11] = L * s2T * E2T - g * IT
 end
 
 """
